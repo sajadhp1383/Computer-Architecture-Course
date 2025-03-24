f8c7c17b5babf1f56cd56578c276287cffe88b01
sajadhp <hasanpoursajad83@gmail.com>
2025-03-24 21:12:57 +0330

    edit Maze-Memory file

--- CA1/Maze-Memory.sv
+++ CA1/Maze-Memory.sv
@@ -1,18 +1,22 @@
 module MAZE_MEM(input clk, RD, WR, Din, input [3:0]X,Y, output logic Dout);
 
-    logic Memory [0:3][0:3];
+    logic Memory [0:15][0:15];
 
     initial begin
         $readmemb("Mem_init.bin", Memory);
     end
 
-    always @(posedge clk) begin
-        if(WR) begin 
+    always_comb begin
+        if (RD)  
+            Dout = Memory[X][Y];  
+        else  
+            Dout = 1'bz;
+    end  
+
+    always_ff @(posedge clk) begin
+        if(WR)
             Memory[X][Y] <= Din;
-        end
-        else if(RD) begin
-            Dout <= Memory[X][Y];
-        end
+        
     end
 
 endmodule
