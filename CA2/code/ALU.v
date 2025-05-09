5c62e0ed829c70fd3f8ade71026a9b0bf4262540
sajadhp <hasanpoursajad83@gmail.com>
2025-05-09 19:40:50 +0330

    set comment for alu operations

--- CA2/code/ALU.v
+++ CA2/code/ALU.v
@@ -6,11 +6,12 @@ module ALU(a,b,op,aluout,zero);
 
     always @(*) begin
         case (op)
-            3'b000: aluout = a + b; 
-            3'b001: aluout = a - b; 
-            3'b010: aluout = a & b; 
-            3'b011: aluout = a | b; 
-            3'b101: aluout = (a < b) ? 1 : 0; 
+            3'b000: aluout = a + b; //add
+            3'b001: aluout = a - b; //sub
+            3'b010: aluout = a & b; //and
+            3'b011: aluout = a | b; //or
+            3'b101: aluout = (a < b) ? 1 : 0; //slti
+            3'b110: aluout = a << b; //sll
             default: aluout = 32'b0; 
         endcase
 
