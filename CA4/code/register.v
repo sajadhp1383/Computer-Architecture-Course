99c756cb69ed79bc0f7a5d2d0614f44a367ff9ae
sajadhp <hasanpoursajad83@gmail.com>
2025-06-10 20:58:39 +0330

    make the register with parameter

--- CA4/code/register.v
+++ CA4/code/register.v
@@ -13,3 +13,21 @@ module Register #(parameter LEN = 32) (in, clk, rst, out);
         end
     end
 endmodule
+
+module EnRegister #(parameter LEN = 32) (in, en, clr, clk, rst, out);
+    input [LEN-1:0] in;
+    input clk;
+    input rst;
+    input en;
+    input clr;
+    output reg [LEN-1:0] out;
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst || clr) begin
+            out <= {LEN{1'b0}};
+        end
+        else if(en)begin             
+            out <= in;
+        end
+    end
+endmodule
