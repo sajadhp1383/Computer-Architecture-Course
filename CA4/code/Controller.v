03ade62d24424ebccfea31728ebcd29fc75b079a
sajadhp <hasanpoursajad83@gmail.com>
2025-06-13 02:31:48 +0330

    change wrong madul ins names

--- CA4/code/Controller.v
+++ CA4/code/Controller.v
@@ -5,7 +5,7 @@ module controller(op, funct3, funct7, ResultSrcD, MemWriteD, ALUControlD,
     input [6:0] op, funct7;
     output wire [1:0] ResultSrcD;                
     output wire [2:0] ALUControlD, ImmSrcD;
-    output wire MemWriteD, ALUSrcD, RegWriteD, sel_adder;
+    output wire MemWriteD, ALUSrcD, RegWriteD, sel_adder, JumpD, BranchD;
 
     ALU_Controller ALU_Controller_inst(
         .funct3(funct3),
@@ -13,7 +13,7 @@ module controller(op, funct3, funct7, ResultSrcD, MemWriteD, ALUControlD,
         .op(op),
         .ALUControlD(ALUControlD)
     );
-    OP_Controller OP_Controller_inst(
+    Control_Unit main_Controller_inst(
         .op(op),
         .funct3(funct3),
         .ResultSrcD(ResultSrcD),
@@ -21,6 +21,8 @@ module controller(op, funct3, funct7, ResultSrcD, MemWriteD, ALUControlD,
         .ALUSrcD(ALUSrcD),
         .ImmSrcD(ImmSrcD),
         .RegWriteD(RegWriteD),
-        .sel_adder(sel_adder)
+        .sel_adder(sel_adder),
+        .JumpD(JumpD),
+        .BranchD(BranchD)
     );
 endmodule
